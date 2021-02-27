cd4319898aa65b4a48b7fa7bd71fcdde359e4d7d
Christine <32354154+kissChriss@users.noreply.github.com>
2021-02-27 14:27:17 +0300

    Add users

--- social_net/social_net/settings.py
+++ social_net/social_net/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'social_net_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'social_net.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- social_net/social_net_app/views.py
+++ social_net/social_net_app/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
+from social_net_app.models import User
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    users = User.objects.all()
+    return render(request, "index.html", context = {'users': users})
